278b2fa87b11098c3085cd9157e23153bd65b332
2022Bliss <blessingirapaul@gmail.com>
2022-07-17 22:29:20 -0700

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,21 +13,21 @@ char *cap_string(char *n)
 	int cap = 32;
 	int separators[] = {',', ';', '.', '?', '"',
 			 '(', ')', '{', '}', ' ', '\n', '\t'};
-					for (i = 0; n[i] != '\0'; i++)
-							{
-										if (n[i] >= 'a' && n[i] <= 'z')
-													{
-																	n[i] = n[i] - cap;
-																			}
-												cap = 0;
-														for (x = 0; x <= 12; x++)
-																	{
-																					if (n[i] == separators[x])
-																									{
-																														x = 12;
-																																		cap = 32;
-																																					}
-																							}
-															}
-						return (n);
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			{
+				n[i] = n[i] - cap;
+			}
+		cap = 0;
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == separators[x])
+				{
+					x = 12;
+					cap = 32;
+				}
+		}
+	}
+	return (n);
 }
